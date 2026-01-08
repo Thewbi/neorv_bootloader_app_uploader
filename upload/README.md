7a625dbe801679d4fead9574f9dd6238f5a2a4d3
thewbi <wfb2@gmx.de>
2026-01-08 10:56:41 +0100

    better error handling

--- upload/README.md
+++ upload/README.md
@@ -1,27 +1,5 @@
-## Todos
+# Uploader
 
-A todos tracker inspired by [TodoMVC]. It showcases dynamic layout, text input, checkboxes, scrollables, icons, and async actions! It automatically saves your tasks in the background, even if you did not finish typing them.
-
-All the example code is located in the __[`main`]__ file.
-
-<div align="center">
-  <a href="https://iced.rs/examples/todos.mp4">
-    <img src="https://iced.rs/examples/todos.gif">
-  </a>
-</div>
-
-You can run the native version with `cargo run`:
-```
-cargo run --package todos
-```
-
-The web version can be run with [`trunk`]:
-
-```
-cd examples/todos
-trunk serve
-```
-
-[`main`]: src/main.rs
-[TodoMVC]: http://todomvc.com/
-[`trunk`]: https://trunkrs.dev/
+This application connects to a COM port and listens for the bootloader output.
+Once it detects the bootloader output it will abort the auto-boot, then it will start an application upload.
+If the bootloader output is not detected within 10 seconds, the application will silently not upload your file!
--- upload/src/main.rs
+++ upload/src/main.rs
@@ -1,22 +1,23 @@
-use std::time::Duration;
-use std::io::{ self, Write, BufRead, BufReader };
 use std::fs;
+use std::io::{self, BufRead, BufReader, Write};
+use std::time::Duration;
 
 use std::path::Path;
 
 use iced::advanced::graphics::core::SmolStr;
-use iced::window;
-use iced::alignment::Alignment::Start;
 use iced::alignment::Alignment::Center;
-use iced::{ Application, Element, Fill, Font, Function, Preset, Program, Subscription, Task, Theme };
-use iced::widget::{ Column, button, column, text, text_input };
+use iced::alignment::Alignment::Start;
+use iced::widget::{Column, button, column, text, text_input};
+use iced::window;
+use iced::{
+    Application, Element, Fill, Font, Function, Preset, Program, Subscription, Task, Theme,
+};
 
 pub fn main() -> iced::Result {
     application().run()
 }
 
 fn application() -> Application<impl Program<Message = Message, Theme = Theme>> {
-
     iced::application(Upload::new, Upload::update, Upload::view)
         .title(Upload::title)
         .window_size((1024.0, 512.0))
@@ -26,13 +27,16 @@ fn application() -> Application<impl Program<Message = Message, Theme = Theme>> 
 pub struct Upload {
     value: i64,
     file_to_upload: SmolStr,
+    serial_port: SmolStr,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     Connect,
-    InputChanged(String),
-    FileToUploadChanged(SmolStr)
+    FileToUploadInputChanged(String),
+    FileToUploadChanged(SmolStr),
+    SerialPortInputChanged(String),
+    SerialPortChanged(SmolStr),
 }
 
 fn subtle_test_style(theme: &Theme) -> text::Style {
@@ -42,7 +46,6 @@ fn subtle_test_style(theme: &Theme) -> text::Style {
 }
 
 impl Upload {
-
     fn title(&self) -> String {
         format!("NEORV32 Bootloader Uploader")
     }
@@ -50,15 +53,18 @@ impl Upload {
     pub fn new() -> Self {
         Self {
             value: 0,
-            file_to_upload: SmolStr::new(r"C:\Users\lapto\dev\VHDL\neorv32-setups\neorv32\sw\example\demo_blink_led\neorv32_exe.bin"),
+
+            file_to_upload: SmolStr::new(
+                r"C:\Users\lapto\dev\VHDL\neorv32-setups\neorv32\sw\example\demo_blink_led\neorv32_exe.bin",
+            ),
             //file_to_upload: SmolStr::new(r"C:\Users\lapto\dev\VHDL\neorv32-setups\neorv32\sw\example\hello_world\neorv32_exe.bin"),
+            serial_port: SmolStr::new("COM5"),
         }
     }
 
     fn update(&mut self, message: Message) {
         match message {
-
-            Message::InputChanged(new_input) => {
+            Message::FileToUploadInputChanged(new_input) => {
                 print!("{}\r\n", new_input);
                 self.file_to_upload = SmolStr::new(new_input.as_str());
             }
@@ -67,27 +73,55 @@ impl Upload {
                 print!("{}\r\n", new_file_to_upload);
             }
 
-            Message::Connect => {
+            Message::SerialPortInputChanged(new_input) => {
+                print!("{}\r\n", new_input);
+                self.serial_port = SmolStr::new(new_input.as_str());
+            }
 
+            Message::SerialPortChanged(serial_port) => {
+                print!("{}\r\n", serial_port);
+            }
+
+            Message::Connect => {
                 let path = Path::new(self.file_to_upload.as_str());
                 if !path.exists() {
                     print!("Path {} does not exist!\r\n", self.file_to_upload.as_str());
                     return;
                 }
 
-                let mut port = serialport::new("COM5", 19_200)
+                // let mut port = serialport::new(self.serial_port.as_str(), 19_200)
+                //     .timeout(Duration::from_millis(10000))
+                //     .open()
+                //     .expect("Failed to open port");
+
+                let open_result = serialport::new(self.serial_port.as_str(), 19_200)
                     .timeout(Duration::from_millis(10000))
-                    .open()
-                    .expect("Failed to open port");
+                    .open();
+
+                let mut port = match open_result {
+                    Ok(port) => port,
+                    Err(error) => {
+                        print!("Cannot open {}\r\n", self.serial_port.as_str());
+                        return;
+                    }
+                };
 
                 let mut string_buffer = String::new();
 
                 let mut done: bool = false;
 
                 while !done {
-
                     let mut serial_buf: Vec<u8> = vec![0; 100];
-                    let bytes_read = port.read(serial_buf.as_mut_slice()).expect("Found no data!");
+                    //let bytes_read = port.read(serial_buf.as_mut_slice()).expect("Found no data!");
+
+                    let read_result = port.read(serial_buf.as_mut_slice());
+                    let mut bytes_read = match read_result {
+                        Ok(bytes_read) => bytes_read,
+                        Err(error) => {
+                            print!("Cannot read from {}\r\n", self.serial_port.as_str());
+                            return;
+                        }
+                    };
 
                     let string_data = String::from_utf8(serial_buf).unwrap();
                     let substring = &string_data[..bytes_read];
@@ -97,7 +131,6 @@ impl Upload {
                     string_buffer.push_str(substring);
 
                     if string_buffer.contains("Auto-boot") {
-
                         string_buffer.clear();
 
                         // wait for the auto-boot to abort
@@ -113,11 +146,9 @@ impl Upload {
                         // select the upload command
                         let output = "u".as_bytes();
                         port.write(output).expect("Write failed!");
-
                     }
 
                     if string_buffer.contains("Awaiting neorv32_exe.bin") {
-
                         string_buffer.clear();
 
                         // wait for upload to be processed
@@ -133,11 +164,9 @@ impl Upload {
                         } else {
                             print!("Path {} does not exist!", self.file_to_upload.as_str());
                         }
-
                     }
 
                     if string_buffer.contains("OK") {
-
                         string_buffer.clear();
 
                         // wait for upload to be processed
@@ -149,9 +178,7 @@ impl Upload {
 
                         // terminate this button click
                         done = true;
-
                     }
-
                 }
 
                 std::mem::drop(port);
@@ -167,8 +194,16 @@ impl Upload {
 
             text_input("File:", &self.file_to_upload)
                     .id("file-to-upload")
-                    .on_input(Message::InputChanged)
+                    .on_input(Message::FileToUploadInputChanged)
                     .on_submit(Message::FileToUploadChanged(SmolStr::new("")))
+                    .padding(15)
+                    .size(12)
+                    .align_x(Start),
+
+            text_input("Serial-Port:", &self.serial_port)
+                    .id("serial-port")
+                    .on_input(Message::SerialPortInputChanged)
+                    .on_submit(Message::SerialPortChanged(SmolStr::new("")))
                     .padding(15)
                     .size(12)
                     .align_x(Start),
@@ -197,7 +232,7 @@ impl Upload {
                     .style(subtle_test_style)
                     .align_x(Start),
 
-            text("5. This uploader will wait 10 seconds for the Bootloader to respond after the reset. If the bootloader does not respond, tis uploader application terminates itself!")
+            text("5. This uploader will wait 10 seconds for the Bootloader to respond after the reset. If the bootloader does not respond, this uploader application will silently fail and NOT UPLOAD your file!")
                     .width(Fill)
                     .size(16)
                     .style(subtle_test_style)
@@ -207,5 +242,4 @@ impl Upload {
         .padding(20)
         .align_x(Center)
     }
-
 }
